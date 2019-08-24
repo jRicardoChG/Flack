2926df944c26d9a177b8d5547ca121384210ed22
jchavez <jaime.chavez@cyxtera.com>
2019-08-23 22:08:22 -0500

    ya estoy terminado diseno

--- main/static/javascript/funciones.js
+++ main/static/javascript/funciones.js
@@ -25,6 +25,10 @@ function recurRem(tag) {
     }
     return;
 }
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
 
 recurAdd("html");
 
