eb2018d2d3703a8bc309f55d70125059aadebe48
ricardo <richago29@gmail.com>
2019-08-27 23:16:55 -0400

    falta cuadrar usuario con localstorage

--- main/static/javascript/funciones.js
+++ main/static/javascript/funciones.js
@@ -1,5 +1,7 @@
 var xhr = new XMLHttpRequest();
+var Dias = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
+// Pruebas de peticiones AJAX con servidor
 function AServer()
 {
     var aServidor = {"saludo":"Hola jefe te puedes mover?, lo demas, el impacto... no hay nada que hacer"};
@@ -24,12 +26,7 @@ function AServer()
       }
       return false;
 }
-function comenzarSocket()
-{
-    var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
-
-}
-
+// funcion que inicializa las peticiones Socketio
 function socketMessage()
 {
     //conectar con el servidor
@@ -38,18 +35,66 @@ function socketMessage()
         console.log("me he conectado");
         document.querySelector("#boton2").addEventListener("click",enviardatos);
     })
-
+    // para poder enviar datos con enter
+    window.addEventListener("keypress",(event) => {
+        if(event.keyCode === 13 && !event.shiftKey)
+        {
+            enviardatos();
+        }
+    });
+    // para que el div de mensajes este siempre abajo y meustre todos los mensajes
+    window.addEventListener("DOMNodeInserted", function () {
+        var scroll = document.querySelector("#chatscroll");
+        scroll.scrollTop = scroll.scrollTop + scroll.scrollHeight;
+    })
+    // realizar el socket.emit con lo que tiene el text area si no esta nulo
     function enviardatos()
     {
-        var seleccion = document.querySelector("#txtmensaje").value;
-        socket.emit("socketMessage",{"seleccion":seleccion});
+        var seleccion = document.querySelector("#txtmensaje");
+        if(seleccion.value == ""|| seleccion.value == null || seleccion.value == undefined)
+        {
+            return;
+        }
+        else
+        {
+            // crea nuevo objeto fecha actualizado
+            var fecha = new Date();
+            // emit de los datos
+            socket.emit("socketMessage",{"seleccion":seleccion.value});
+            // creacion del mensaje propio en pantalla
+            let mensajeCaja = document.createElement("div");
+            mensajeCaja.innerHTML = '<div class="mOwn global"><p class="global timestamp"></p><p class="global"></p></div>';
+            mensajeCaja.classList.add("flexMessageRight");
+            mensajeCaja.classList.add("global");
+            mensajeCaja.children[0].children[1].innerHTML = seleccion.value;
+            let chat = document.querySelector("#chatscroll");
+            
+            mensajeCaja.children[0].children[0].innerHTML = Dias[fecha.getDay()] +" "+ fecha.getHours() +":"+ fecha.getMinutes() +":"+ fecha.getSeconds() +" "+ "Ricardo";
+            chat.appendChild(mensajeCaja);
+            seleccion.value = "";
+        }
     }
+
     socket.on("mensajeServer", mensaje => {
-        console.log(mensaje["seleccion"]);
-    } )
+        var recibido = mensaje["seleccion"];
+        console.log(recibido);
+        if(recibido == ""|| recibido == null || recibido == undefined)
+        {
+            return;
+        }
+        else
+        {
+            let mensajeCaja = document.createElement("div");
+            mensajeCaja.innerHTML = '<div class="mCom global"><p class="global"></p></div>';
+            mensajeCaja.classList.add("flexMessageLeft");
+            mensajeCaja.classList.add("global");
+            mensajeCaja.children[0].children[0].innerHTML = recibido;
+            let chat = document.querySelector("#chatscroll");
+            chat.appendChild(mensajeCaja);
+        }
+    })
 
 }
-
 
 // funcion recursiva para aplicar clase global a todos los elementos
 function recurAdd(tag) {
@@ -65,23 +110,22 @@ function recurAdd(tag) {
     return;
 }
 
-// funcion recursiva para quitarlas
-function recurRem(tag) {
-    var temp = document.querySelector(tag);
-    temp.classList.remove("global");
-    if (!temp.children.length > 0) {
-        return;
-    } else {
-        for (var i = 0; i < temp.children.length; i++) {
-            recurRem(tag + ">" + ":nth-child(" + Number(i + 1) + ")");
-        }
-    }
-    return;
-}
+// funcion para aumentar tamaño del textarea
 function auto_grow(element) {
     element.style.height = "70px";
     element.style.height = element.scrollHeight+"px";
 }
 
+// funcion para poner div que obliga al usuario a crear un usuario, si ya esta guardado en localstorage no aparece
+function divUsuario () {
+    var divGeneral = document.querySelector("#ingresar");
+    divGeneral.setAttribute("style","width:100vw; height:100vh; background-color:#F2F2F2; z-index:100; display:flex; flex-direccion:column; justify-content:center; align-items:center;");
+    divGeneral.innerHTML = "<div><p>Hola ingresa un usuario para participar:</p><input type='text' placeholder='nombre de usuario'></input></div>"
+    divGeneral.children[1].style.width="100%";
+}
+
+//ejecucion inmediata de funciones
 recurAdd("html");
+divUsuario();
+socketMessage();
 
