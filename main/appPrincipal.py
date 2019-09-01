d38e06c80fa42878b211dda181bae19ea142856f
ricardo <richago29@gmail.com>
2019-08-31 23:06:10 -0400

    tengo uqe terminar de hacer el filtro de canalesglobales

--- main/appPrincipal.py
+++ main/appPrincipal.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import re
 
 from funciones          import organizar
 from flask              import Flask, session,render_template,request,jsonify
@@ -19,6 +20,9 @@ app.config["SECRET_KEY"] = "secretisima!"
 socketio = SocketIO(app)
 ###############################################################################################################
 
+# En esta variable voy a guardar todos los datos de la aplicacion
+DatosApp = {"botas":{},"botines":{},"besos":{},"bufandas":{},"carros":{},"choques":{}}
+
 @app.route("/",methods=["GET","POST"])
 def pagPrincipal():
     if request.method == "POST":
@@ -26,6 +30,40 @@ def pagPrincipal():
         print("lo uqe recibi",mensaje)
         return jsonify({"respuesta":"te he respondido","texto":mensaje})
     return render_template("home.html")
+
+
+
+@app.route("/buscarCanalGlobal",methods=["POST"])
+def buscarGlobal():
+    if request.method == "POST":
+        mensaje = request.form.get("canalGlobal")
+        retorno = []
+        for llaves in DatosApp.keys():
+            if re.search('"'+mensaje+'"',llaves):
+                retorno[len(retorno):]=[llaves]
+        return jsonify({"canales":retorno})    
+
+
+
+@app.route("/crearCanal",methods=["POST"])
+def crearCanal():
+    if request.method == "POST":
+        temproom = request.form.get("nuevoCanal")
+        if (temproom not in DatosApp.keys()):
+            DatosApp[temproom] = {}
+            DatosApp[temproom]["dueno"] = request.form.get("dueno")
+            print(DatosApp)
+            return jsonify({"respuesta":"se ha creado  una room"})
+        else:
+            return jsonify({"respuesta":"La room ya existe"}) 
+
+
+
+@socketio.on("connect")
+def test_connect():
+    emit("testConexion",{"conexion":"Te has conectado, te he detectado pasame tu room actual"})
+
+
 
 @socketio.on("socketMessage")
 def socket(datos):
--- main/static/javascript/funciones.js
+++ main/static/javascript/funciones.js
@@ -1,99 +1,87 @@
 var xhr = new XMLHttpRequest();
-var Dias = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var Dias = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var canalesglobales = document.querySelector("#canalesGlobales").children[1];
+
+////////////////////////////////////////////////MAIN CODE/////////////////////////////////////////////////////////////////
+// Conectar con el servidor
+var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
+socket.on("connect", mensaje => {
+    console.log("me he conectado");
+    document.querySelector("#boton2").addEventListener("click", enviardatos);
+})
+// Test de coenxion con el servidor
+socket.on("testConexion", mensaje => {
+    console.log(mensaje["conexion"]);
+})
+// Cuando se envia un mensaje en el chat
+socket.on("mensajeServer", mensaje => {
+    var recibido = mensaje["seleccion"];
+    console.log(recibido);
+    if (recibido == "" || recibido == null || recibido == undefined) {
+        return;
+    }
+    else {
+        let mensajeCaja = document.createElement("div");
+        mensajeCaja.innerHTML = '<div class="mCom global"><p class="global"></p></div>';
+        mensajeCaja.classList.add("flexMessageLeft");
+        mensajeCaja.classList.add("global");
+        mensajeCaja.children[0].children[0].innerHTML = recibido;
+        let chat = document.querySelector("#chatscroll");
+        chat.appendChild(mensajeCaja);
+    }
+})
+// Para poder enviar datos con enter
+window.addEventListener("keypress", (event) => {
+    if (event.keyCode === 13 && !event.shiftKey) {
+        enviardatos();
+    }
+});
+// Para que el div de mensajes este siempre abajo y meustre todos los mensajes
+window.addEventListener("DOMNodeInserted", function () {
+    var scroll = document.querySelector("#chatscroll");
+    scroll.scrollTop = scroll.scrollTop + scroll.scrollHeight;
+})
+// para que al dar click se cree un canal
+var botonCrearCanal = document.querySelector("#btnCrearCanal").addEventListener("click",crearCanal);
+var botonBuscarGlobal = document.querySelector("#btnBuscarCanalGlobal").addEventListener("click",buscarCanalglobal);
+
+xhr.onreadystatechange = function(){
+    if (xhr.readyState == 4 && xhr.status==202)
+    {
+        respuesta = xhr.responseText
+        if (respuesta["canales"])
+        {
+            for (canales in respuesta["canales"])
+            {
+                var hijo = document.createElement("div");
+                hijo.innerHTML = "<p>"+canales+"</p>"
+                canalesglobales.appendChild(hijo)
+            }
+        }
+    }
+}
+/////////////////////////////////////////////////FUNCTIONS////////////////////////////////////////////////////////////////
 
 // Pruebas de peticiones AJAX con servidor
-function AServer()
-{
-    var aServidor = {"saludo":"Hola jefe te puedes mover?, lo demas, el impacto... no hay nada que hacer"};
-    xhr.open("POST","/",true);
+function AServer() {
+    var aServidor = { "saludo": "Hola jefe te puedes mover?, lo demas, el impacto... no hay nada que hacer" };
+    xhr.open("POST", "/", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     // Send request
-    xhr.send(Object.keys(aServidor)[0]+"="+aServidor[Object.keys(aServidor)[0]]);
+    xhr.send(Object.keys(aServidor)[0] + "=" + aServidor[Object.keys(aServidor)[0]]);
 
     xhr.onload = () => {
         var respuesta = JSON.parse(xhr.responseText);
         // Update the result div
         if (respuesta) {
-            var algo = document.createElement("p")
-            algo.innerHTML = Object.keys(respuesta)[1]+": "+respuesta[Object.keys(respuesta)[1]];
-            document.body.appendChild(algo)
+            console.log("ServerContesta: "+respuesta);
         }
-        else
-        {
-          console.log("no tengo nada")  
-        }
-      }
-      return false;
-}
-// funcion que inicializa las peticiones Socketio
-function socketMessage()
-{
-    //conectar con el servidor
-    var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
-    socket.on("connect", function(){
-        console.log("me he conectado");
-        document.querySelector("#boton2").addEventListener("click",enviardatos);
-    })
-    // para poder enviar datos con enter
-    window.addEventListener("keypress",(event) => {
-        if(event.keyCode === 13 && !event.shiftKey)
-        {
-            enviardatos();
-        }
-    });
-    // para que el div de mensajes este siempre abajo y meustre todos los mensajes
-    window.addEventListener("DOMNodeInserted", function () {
-        var scroll = document.querySelector("#chatscroll");
-        scroll.scrollTop = scroll.scrollTop + scroll.scrollHeight;
-    })
-    // realizar el socket.emit con lo que tiene el text area si no esta nulo
-    function enviardatos()
-    {
-        var seleccion = document.querySelector("#txtmensaje");
-        if(seleccion.value == ""|| seleccion.value == null || seleccion.value == undefined)
-        {
-            return;
-        }
-        else
-        {
-            // crea nuevo objeto fecha actualizado
-            var fecha = new Date();
-            // emit de los datos
-            socket.emit("socketMessage",{"seleccion":seleccion.value});
-            // creacion del mensaje propio en pantalla
-            let mensajeCaja = document.createElement("div");
-            mensajeCaja.innerHTML = '<div class="mOwn global"><p class="global timestamp"></p><p class="global"></p></div>';
-            mensajeCaja.classList.add("flexMessageRight");
-            mensajeCaja.classList.add("global");
-            mensajeCaja.children[0].children[1].innerHTML = seleccion.value;
-            let chat = document.querySelector("#chatscroll");
-            
-            mensajeCaja.children[0].children[0].innerHTML = Dias[fecha.getDay()] +" "+ fecha.getHours() +":"+ fecha.getMinutes() +":"+ fecha.getSeconds() +" "+ "Ricardo";
-            chat.appendChild(mensajeCaja);
-            seleccion.value = "";
+        else {
+            console.log("no tengo nada")
         }
     }
-
-    socket.on("mensajeServer", mensaje => {
-        var recibido = mensaje["seleccion"];
-        console.log(recibido);
-        if(recibido == ""|| recibido == null || recibido == undefined)
-        {
-            return;
-        }
-        else
-        {
-            let mensajeCaja = document.createElement("div");
-            mensajeCaja.innerHTML = '<div class="mCom global"><p class="global"></p></div>';
-            mensajeCaja.classList.add("flexMessageLeft");
-            mensajeCaja.classList.add("global");
-            mensajeCaja.children[0].children[0].innerHTML = recibido;
-            let chat = document.querySelector("#chatscroll");
-            chat.appendChild(mensajeCaja);
-        }
-    })
-
+    return false;
 }
 
 // funcion recursiva para aplicar clase global a todos los elementos
@@ -113,19 +101,83 @@ function recurAdd(tag) {
 // funcion para aumentar tamaño del textarea
 function auto_grow(element) {
     element.style.height = "70px";
-    element.style.height = element.scrollHeight+"px";
+    element.style.height = element.scrollHeight + "px";
 }
 
 // funcion para poner div que obliga al usuario a crear un usuario, si ya esta guardado en localstorage no aparece
-function divUsuario () {
+function divUsuario() {
     var divGeneral = document.querySelector("#ingresar");
-    divGeneral.setAttribute("style","width:100vw; height:100vh; background-color:#F2F2F2; z-index:100; display:flex; flex-direccion:column; justify-content:center; align-items:center;");
-    divGeneral.innerHTML = "<div><p>Hola ingresa un usuario para participar:</p><input type='text' placeholder='nombre de usuario'></input></div>"
-    divGeneral.children[1].style.width="100%";
+    if (localStorage.getItem("usuarioActual") == undefined) {
+        divGeneral.setAttribute("style", "position:absolute; top:0px; left:0px; width:100vw; height:100vh; background-color:#F2F2F2; z-index:100; display:flex; flex-direccion:column; justify-content:center; align-items:center;");
+        divGeneral.innerHTML = "<div><p>Hola ingresa un usuario para participar:</p><input type='text' placeholder=' Nombre de usuario'></input><button>Entrar</button></div>"
+        divGeneral.children[0].children[1].style.width = "100%";
+        divGeneral.children[0].children[2].addEventListener("click", guardarDatos);
+    }
+    else {
+        divGeneral.setAttribute("style", "display:none;");
+    }
+    function guardarDatos() {
+        if (divGeneral.children[0].children[1].value != undefined && divGeneral.children[0].children[1].value != "") {
+            localStorage.setItem("usuarioActual", divGeneral.children[0].children[1].value);
+            divGeneral.setAttribute("style", "display:none;");
+        }
+        else
+            return;
+    }
 }
+
+// Peticion para crear una room
+function crearCanal() {
+    var nombreCanal = document.querySelector("#newChan");
+    if(nombreCanal.value != undefined && nombreCanal.value != "" && nombreCanal.value != null)
+    {
+        xhr.open("POST", "/crearCanal", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send("nuevoCanal="+nombreCanal.value+"&dueno="+localStorage.getItem("usuarioActual"));
+    }
+}
+// peticion para buscar todos los canales existentes
+function buscarCanalglobal(){
+    var nombreCanal = document.querySelector("#buscarCanalGlobal");
+    if(nombreCanal.value != undefined && nombreCanal.value != "" && nombreCanal.value != null)
+    {
+        xhr.open("POST", "/buscarCanalGlobal", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send("canalGlobal="+nombreCanal.value);
+    }
+    else
+        return;
+}
+
+
+// Realizar el socket.emit con lo que tiene el text area si no esta nulo
+function enviardatos() {
+    var seleccion = document.querySelector("#txtmensaje");
+    if (seleccion.value == "" || seleccion.value == null || seleccion.value == undefined) {
+        return;
+    }
+    else {
+        // crea nuevo objeto fecha actualizado
+        var fecha = new Date();
+        // emit de los datos
+        socket.emit("socketMessage", { "seleccion": seleccion.value });
+        // creacion del mensaje propio en pantalla
+        let mensajeCaja = document.createElement("div");
+        mensajeCaja.innerHTML = '<div class="mOwn global"><p class="global timestamp"></p><p class="global"></p></div>';
+        mensajeCaja.classList.add("flexMessageRight");
+        mensajeCaja.classList.add("global");
+        mensajeCaja.children[0].children[1].innerHTML = seleccion.value;
+        let chat = document.querySelector("#chatscroll");
+
+        mensajeCaja.children[0].children[0].innerHTML = Dias[fecha.getDay()] + " " + fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds() + " " + localStorage.getItem("usuarioActual");
+        chat.appendChild(mensajeCaja);
+        seleccion.value = "";
+    }
+}
+
+
 
 //ejecucion inmediata de funciones
 recurAdd("html");
 divUsuario();
-socketMessage();
 
